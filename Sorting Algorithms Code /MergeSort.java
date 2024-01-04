f44e02ec8706b37e0677a77fd9b2feb5e7bbe851
-=rBeriwal=- <119040957+Rohanberiwal@users.noreply.github.com>
2024-01-04 16:09:04 +0530

    Create MergeSort.java

---
+++ Sorting Algorithms Code /MergeSort.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class MergeSort {
+    public static void mergeSort(int[] array) {
+        if (array.length > 1) {
+            int mid = array.length / 2;
+            int[] left = Arrays.copyOfRange(array, 0, mid);
+            int[] right = Arrays.copyOfRange(array, mid, array.length);
+            mergeSort(left);
+            mergeSort(right);
+
+            merge(array, left, right);
+        }
+    }
+
+    private static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Original array: " + Arrays.toString(array));
+
+        mergeSort(array);
+
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+}
