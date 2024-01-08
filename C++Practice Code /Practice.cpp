95313e701f29a64605bbb1f106f93953f9339c44
-=rBeriwal=- <119040957+Rohanberiwal@users.noreply.github.com>
2024-01-08 21:18:39 +0530

    Update Practice.cpp

--- C++Practice Code /Practice.cpp
+++ C++Practice Code /Practice.cpp
@@ -7,3 +7,94 @@ int main()
     return 0 ;
     
 }
+#include <iostream>
+// endl escape  sequence 
+using namespace std ;
+int main()
+{
+    cout<<"\nhello world " ; 
+    cout<<endl ;
+    cout<<"\n this is the next line " ;
+    return 0 ;
+    
+}
+
+
+#include <iostream>
+// endl escape  sequence in the single line 
+using namespace std ;
+int main()
+{
+    cout<<"\nhello world " <<endl  ;
+    cout<<"\n this is the next line " ;
+    return 0 ;
+    
+}
+
+#include <iostream>
+// endl escape  sequence in the single line 
+using namespace std ;
+int main()
+{
+   int x  = 10; // initlisation of the variable 
+   cout<<"the number is "<<x  ;
+   return 0 ;
+}
+
+#include <iostream>
+// endl escape  sequence in the single line 
+using namespace std ;
+int main()
+{
+   int x  = 10; // initlisation of the variable 
+   float y  = 100.87 ;
+   cout<<"the number is "<<x  ;
+   cout<< endl ;
+   cout<< "the number y is "<<y ;
+   return 0 ;
+}
+
+#include <iostream>
+// endl escape  sequence in the single line 
+using namespace std ;
+int main()
+{
+   int x  = 10; // initlisation of the variable 
+   float y  = 100.87 ;
+   cout<<"computation " <<x*y ;
+   cout <<endl ;
+   return 0 ;
+}
+
+// conditionals 
+#include <iostream>
+using namespace std  ;
+int main()
+{
+    int x = 10 ;
+    if(x==10)
+    {
+        cout<<"True the number is " <<x ;
+        
+    }
+    else 
+    {
+     cout<<"the number is not " <<x ;
+    }
+    return 0 ;
+}
+
+// conditonals wiht the boolean operators 
+#include <iostream>
+#include <climits> 
+using namespace std  ;
+int main()
+{
+   int x  = INT_MAX ;
+   bool check  = true  ;
+   if(x> 0)
+   {
+       check  = false ;
+   }
+   cout<<"check is "<<check ;
+}
