7e9e43836854546a72ac3a3eab230052ee8561f5
gravity.inescapable <119040957+Rohanberiwal@users.noreply.github.com>
2024-03-11 09:19:08 +0530

    Create Lexographical.py
    Lexogrpahical sorting using  the randomized quicksort algo .

---
+++ Sorting Algorithms Code /Lexographical.py
@@ -0,0 +1,19 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quicksort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quicksort(arr, low, pi - 1)
+        quicksort(arr, pi + 1, high)
+
+strings = ['banana', 'apple', 'orange', 'grape']
+quicksort(strings, 0, len(strings) - 1)
+print(strings)
